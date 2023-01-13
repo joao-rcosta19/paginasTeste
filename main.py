9d9fe4d2e5bf60079d01d8e9d6ae5032be5124a9
joao-rcosta19 <joao.rcosta19@gmail.com>
2023-01-12 22:18:26 -0300

    atualização do codigo

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from typing import List, Optional
+
+from infra.config.database import buscar, atualizaSitEmail, conectar, close, inserindoEmail, buscarsituacao
+from infra.repositorios.repositoriosFuncs import validaCampo, validaCampoEmail, validaEmail, enviaremail
+
+
+app = FastAPI()
+
+class requisicaoEmail(BaseModel):
+    chave: int
+    assunto: str
+    texto: str
+    endereco: str
+
+lista: List[requisicaoEmail] = []
+
+@app.post('/listar')
+def criar_requisicao(requisicaoEmail: requisicaoEmail):
+    lista.append(requisicaoEmail)
+    return  lista
